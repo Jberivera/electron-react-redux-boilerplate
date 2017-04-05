9cf551f5bf339075d067fb9e9546f2d7d71f1e55
Juan Bernardo <jberivera@gmail.com>
2017-04-04 22:09:56 -0500

    travis and config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const path = require('path');
-const merge = require('webpack-merge');
 
 const TARGET = process.env.npm_lifecycle_event;
+const DEFAULT_PORT = process.env.PORT || 3000;
 const PATHS = {
   ui: path.join(__dirname, 'app/ui'),
   public: path.join(__dirname, 'app/public')
@@ -16,27 +17,70 @@ const common = {
     filename: 'index.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.scss$/,
-        loader: 'style!css?sourceMap&modules!postcss!sass?sourceMap',
+        use: [
+          'style-loader',
+          'css-loader?sourceMap&modules',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('autoprefixer')({ browsers: ['last 2 versions'] })
+                ];
+              }
+            }
+          },
+          'sass-loader?sourceMap'
+        ],
         include: PATHS.ui
       },
       {
         test: /\.js$/,
-        loaders: ['babel?cacheDirectory'],
+        loader: 'babel-loader?cacheDirectory',
         include: PATHS.ui
       }
     ]
   }
 };
 
-module.exports = merge(common, {
+module.exports = Object.assign(common, {
   start: {
-    devtool: 'eval-source-map'
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT || DEFAULT_PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': '"development"' }})
+    ]
   },
   build: {
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loaders: ['babel?cacheDirectory'],
+          include: PATHS.ui
+        },
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract('style!css?modules!postcss!sass', 'css?modules!postcss!sass'),
+          include: PATHS.ui
+        }
+      ]
+    },
     plugins: [
+      new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': '"production"' }}),
+      new ExtractTextPlugin('css/[name].css?[hash]-[chunkhash]-[contenthash]-[name]', { allChunks: true }),
       new webpack.optimize.UglifyJsPlugin()
     ]
   }
